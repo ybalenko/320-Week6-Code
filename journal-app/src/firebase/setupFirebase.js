c2fd36cc99b391187d6b64a4c77b2ed454c6efb4
Brian Hague <bhague1281@gmail.com>
2022-05-19 16:55:05 -0700

    db

--- journal-app/src/firebase/setupFirebase.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase';
-// Required for side-effects
-import 'firebase/firestore';
-
-firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
-});
